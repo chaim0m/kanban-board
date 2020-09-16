b8e697898174b72fb9e1262d764f988adc8d1693
Chaim <chaim@doit-intl.com>
2020-09-16 21:21:55 +0300

    move delete and validation functionality added

--- src/components/kanban-board/index.js
+++ src/components/kanban-board/index.js
@@ -12,22 +12,52 @@ export default class KanbanBoard extends Component {
         { name: "2", stage: 0 },
       ],
       stagesTasks: [],
-      input: ""
+      input: "",
     };
     this.stagesNames = ["Backlog", "To Do", "Ongoing", "Done"];
-    this.handleClick = this.handleClick.bind(this);
-    this.updateTasksView = this.updateTasksView.bind(this)
-    this.updateInput = this.updateInput.bind(this)
+    this.addNewTask = this.addNewTask.bind(this);
+    this.updateTasksView = this.updateTasksView.bind(this);
+    this.updateInput = this.updateInput.bind(this);
+    this.moveTask = this.moveTask.bind(this);
+    this.removeTask = this.removeTask.bind(this)
   }
-  handleClick = (e) => {
-    this.setState({ tasks: [...this.state.tasks, {name: this.state.input, stage: 0}] }, this.updateTasksView);
-    
+  addNewTask = (e) => {
+    if (this.state.input){
+      this.setState(
+        { tasks: [...this.state.tasks, { name: this.state.input, stage: 0 }],
+          input: ""
+        },
+        this.updateTasksView
+      );
+    }
   };
-  updateInput = e => {
-    this.setState({input: e.target.value})
+  // Task name input
+  updateInput = (e) => {
+    this.setState({ input: e.target.value });
   };
+  // Move task between boards
+  moveTask(direction, currentTaskName) {
+    let currentTaskIndex = this.state.tasks.findIndex(t => t.name === currentTaskName);
+    let operator = 1;
+    if (direction === "back") {
+      operator = -1;
+    }
+      let tasks = [];
+      tasks = [...this.state.tasks];
+      let task = { ...tasks[currentTaskIndex] };
+      task.stage = task.stage + operator;
+      tasks[currentTaskIndex] = task;
+      this.setState({ tasks }, this.updateTasksView);
+  }
+  removeTask(taskName){
+    let currentTaskIndex = this.state.tasks.findIndex(t => t.name === taskName);
+    this.setState({
+      tasks: this.state.tasks.filter((_, i) => i !== currentTaskIndex)
+    }, this.updateTasksView);
+  }
+  // Used for updating the view
   updateTasksView = () => {
-    let stages = []
+    let stages = [];
     for (let i = 0; i < this.stagesNames.length; ++i) {
       stages.push([]);
     }
@@ -35,14 +65,13 @@ export default class KanbanBoard extends Component {
       const stageId = task.stage;
       stages[stageId].push(task);
     }
-    this.setState({stagesTasks: stages})
-  }
-  componentDidMount(){
-    this.updateTasksView()
+    this.setState({ stagesTasks: stages });
+  };
+  componentDidMount() {
+    this.updateTasksView();
   }
   render() {
-    const { stagesTasks } = this.state;
-
+    const { stagesTasks, input } = this.state;
     return (
       <div className="mt-20 layout-column justify-content-center align-items-center">
         <section className="mt-50 layout-row align-items-center justify-content-center">
@@ -52,13 +81,14 @@ export default class KanbanBoard extends Component {
             className="large"
             placeholder="New task name"
             data-testid="create-task-input"
+            value={input}
             onChange={this.updateInput}
           />
           <button
             type="submit"
             className="ml-30"
             data-testid="create-task-button"
-            onClick={this.handleClick}
+            onClick={this.addNewTask}
           >
             Create task
           </button>
@@ -88,6 +118,8 @@ export default class KanbanBoard extends Component {
                                 data-testid={`${task.name
                                   .split(" ")
                                   .join("-")}-back`}
+                                  disabled={task.stage === 0}
+                                  onClick={() => this.moveTask("back", task.name)}
                               >
                                 <i className="material-icons">arrow_back</i>
                               </button>
@@ -96,6 +128,8 @@ export default class KanbanBoard extends Component {
                                 data-testid={`${task.name
                                   .split(" ")
                                   .join("-")}-forward`}
+                                  disabled={task.stage === stagesTasks.length -1}
+                                onClick={() => this.moveTask("forward", task.name)}
                               >
                                 <i className="material-icons">arrow_forward</i>
                               </button>
@@ -104,6 +138,7 @@ export default class KanbanBoard extends Component {
                                 data-testid={`${task.name
                                   .split(" ")
                                   .join("-")}-delete`}
+                                  onClick={() => this.removeTask(task.name)}
                               >
                                 <i className="material-icons">delete</i>
                               </button>
